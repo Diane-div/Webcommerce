b652479a6b9c2537558d5f1dbb191f9adcaa497e
Diane-div <divyanshi_230178@aitpune.edu.in>
2025-08-30 23:41:47 +0530

    TaskList added

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist'className='overflow-x-auto flex items-center justify-start gap-5 flex-nowrap h-[60%] w-full py-5 mt-8'>
+        <div className='flex-shrink-0  h-full w-[300px] bg-amber-300 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0  h-full w-[300px] bg-amber-300 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0  h-full w-[300px] bg-amber-300 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0  h-full w-[300px] bg-amber-300 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0  h-full w-[300px] bg-amber-300 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0  h-full w-[300px] bg-amber-300 rounded-xl'>
+
+        </div>
+        <div className='flex-shrink-0  h-full w-[300px] bg-amber-300 rounded-xl'>
+
+        </div>
+    </div>
+  )
+}
+
+export default TaskList
